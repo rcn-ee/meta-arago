dbd26eb4fbac6050dcd1787d32065e863fef9015
Franklin S. Cooper Jr <fcooper@ti.com>
2014-08-14 01:46:07 +0000

    tslib: Use custom tslib.sh
    * Tslib.sh is copied to /etc/profile.d/ to set default environment variables
      specific to touchscreen.
    * However, in Arago additional env variable logic for touchscreen is required
      and is currently located within Matrix's init script.
    * Pull the additional logic from Matrix's init script and move it into this file
      so tslib.sh will set the correct environment variable globally.
    
    Signed-off-by: Franklin S. Cooper Jr <fcooper@ti.com>
    Signed-off-by: Denys Dmytriyenko <denys@ti.com>

---
+++ meta-arago-distro/recipes-graphics/tslib/files/tslib.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+### Touchscreen Environment Variables ###
+
+if [ ! -e /dev/input/touchscreen0 ]; then
+    exit
+fi
+
+export TSLIB_TSDEVICE=/dev/input/touchscreen0
+
+mount | grep /run/media/mmcblk0p1 | grep vfat > /dev/null 2>&1
+if [ "$?" = "0" ]
+then
+    export TSLIB_CALIBFILE=/run/media/mmcblk0p1/pointercal
+else
+    export TSLIB_CALIBFILE=/etc/pointercal
+fi
---
+++ meta-arago-distro/recipes-graphics/tslib/tslib_1.1.bbappend
@@ -0,0 +1,4 @@
+# look for files in this layer first
+FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
+
+PR_append = "-arago0"
