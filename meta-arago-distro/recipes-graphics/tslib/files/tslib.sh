fc19a68374c89a7dcf397bb1d86368879040eb85
Denys Dmytriyenko <denys@ti.com>
2014-08-26 01:05:48 +0000

    tslib: fix "unable to login" issue
    Scripts in /etc/profile.d/* are being sourced, not executed, so they can't
    contain "exit" commands, otherwise the entire login session exits!
    
    Signed-off-by: Denys Dmytriyenko <denys@ti.com>

--- meta-arago-distro/recipes-graphics/tslib/files/tslib.sh
+++ meta-arago-distro/recipes-graphics/tslib/files/tslib.sh
@@ -2,16 +2,14 @@
 
 ### Touchscreen Environment Variables ###
 
-if [ ! -e /dev/input/touchscreen0 ]; then
-    exit
-fi
+if [ -e /dev/input/touchscreen0 ]; then
+    export TSLIB_TSDEVICE=/dev/input/touchscreen0
 
-export TSLIB_TSDEVICE=/dev/input/touchscreen0
-
-mount | grep /run/media/mmcblk0p1 | grep vfat > /dev/null 2>&1
-if [ "$?" = "0" ]
-then
-    export TSLIB_CALIBFILE=/run/media/mmcblk0p1/pointercal
-else
-    export TSLIB_CALIBFILE=/etc/pointercal
+    mount | grep /run/media/mmcblk0p1 | grep vfat > /dev/null 2>&1
+    if [ "$?" = "0" ]
+    then
+        export TSLIB_CALIBFILE=/run/media/mmcblk0p1/pointercal
+    else
+        export TSLIB_CALIBFILE=/etc/pointercal
+    fi
 fi
