38d7531967c72093cfa29cf7f2d0be1dd459457b
Franklin S. Cooper Jr <fcooper@ti.com>
2014-09-03 01:07:21 +0000

    qtbase: Remove references of TSLIB
    * Since Weston handles touch events in Qt 5 when using Wayland and Weston
      remove references to tslib since its no longer used.
    
    Signed-off-by: Franklin S. Cooper Jr <fcooper@ti.com>
    Signed-off-by: Denys Dmytriyenko <denys@ti.com>

--- meta-arago-distro/recipes-qt/qt5/qtbase/qt_env.sh
+++ meta-arago-distro/recipes-qt/qt5/qtbase/qt_env.sh
@@ -3,10 +3,3 @@
 ### QT Environment Variables ###
 export QT_QPA_GENERIC_PLUGINS=Auto
 export QT_QPA_PLATFORM=wayland
-
-
-# Set the QT_QPA_GENERIC_PLUGINS for touchscreen if it exists
-if [ -e /dev/input/touchscreen0 ]
-then
-    export QT_QPA_GENERIC_PLUGINS=Tslib:/dev/input/touchscreen0
-fi
--- meta-arago-distro/recipes-qt/qt5/qtbase_5.2.1.bbappend
+++ meta-arago-distro/recipes-qt/qt5/qtbase_5.2.1.bbappend
@@ -5,7 +5,7 @@ GLES_EXTRA_DEPS_omap-a15 = "libdrm wayland"
 
 PACKAGECONFIG[gles2] = "-opengl es2 -eglfs,,virtual/libgles2 virtual/egl ${GLES_EXTRA_DEPS}"
 
-PR_append = "-arago2"
+PR_append = "-arago3"
 
 QT_CONFIG_FLAGS += "-qpa wayland"
 
