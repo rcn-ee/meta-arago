6d29db6bf39aa67ff9b5344f2243c084a5a8e857
Karthik Ramanan <a0393906@ti.com>
2015-04-10 18:51:02 +0000

    weston: Add custom profile into targetfs
    There are different set of requirements on the bootup behavior
    across different platforms. To name a few:
     * J6, the required behavior is console/command prompt
     * AM57xx, it will be the matrix-gui (QT5 on Wayland)
    
    weston-init recipe addresses the needs of AM57xx.
    This patch is needed for J6/J6-Eco etc.
    
    Note that if there are customers on AM57xx who need the default behavior
    to be console/command prompt, this patch will address those needs.
    It does not conflict with weston-init.
    
    Signed-off-by: Karthik Ramanan <a0393906@ti.com>
    Signed-off-by: Denys Dmytriyenko <denys@ti.com>

--- meta-arago-distro/recipes-graphics/wayland/weston/wayland_env.sh
+++ meta-arago-distro/recipes-graphics/wayland/weston/wayland_env.sh
@@ -3,6 +3,11 @@
 ### Wayland Environment Variables ###
 
 export XDG_CONFIG_HOME=/etc/
-export XDG_RUNTIME_DIR=/run/user/root
+export XDG_RUNTIME_DIR=/tmp/${UID}-runtime-dir
 export WAYLAND_DISPLAY=wayland-0
 export WS_CALUDEV_FILE=/etc/udev/rules.d/ws-calibrate.rules
+
+if ! test -d "${XDG_RUNTIME_DIR}"; then
+       mkdir -p "${XDG_RUNTIME_DIR}"
+       chmod 0700 "${XDG_RUNTIME_DIR}"
+fi
--- meta-arago-distro/recipes-graphics/wayland/weston_1.6.0.bbappend
+++ meta-arago-distro/recipes-graphics/wayland/weston_1.6.0.bbappend
@@ -2,7 +2,7 @@
 PACKAGECONFIG[fbdev] = "--enable-fbdev-compositor WESTON_NATIVE_BACKEND="fbdev-backend.so",--disable-fbdev-compositor,udev mtdev"
 PACKAGECONFIG[kms] = "--enable-drm-compositor,--disable-drm-compositor,drm udev libgbm mtdev"
 
-PR_append = "-arago3"
+PR_append = "-arago4"
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
 
