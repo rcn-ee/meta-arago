070325e79b8232d9a53e114cccae44b897222475
Gou, Hongmei <h-gou@ti.com>
2017-01-16 22:19:27 +0000

    opencv: update SRC_URI to use tiopencv repo which has TI specific patches
    * The major addition of tiopencv is dispatching OpenCV kernels to DSP via OpenCL
    
    Signed-off-by: Hongmei Gou <h-gou@ti.com>
    Signed-off-by: Denys Dmytriyenko <denys@ti.com>

---
+++ meta-arago-distro/recipes-support/opencv/opencv/am57xx-evm/setupEnv.sh
@@ -0,0 +1,3 @@
+export OPENCV_BUILDDIR=/usr/share/OpenCV/samples
+export OPENCV_TEST_DATA_PATH=/usr/share/OpenCV/testdata
+export OPENCV_OPENCL_DEVICE='TI AM57:ACCELERATOR:TI Multicore C66 DSP'
---
+++ meta-arago-distro/recipes-support/opencv/opencv/setupEnv.sh
@@ -0,0 +1,2 @@
+export OPENCV_BUILDDIR=/usr/share/OpenCV/samples
+export OPENCV_TEST_DATA_PATH=/usr/share/OpenCV/testdata
