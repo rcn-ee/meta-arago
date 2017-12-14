2e51385d27e33a8c0ccc681629d3389cd8aa602a
Jean-Jacques Hiblot <jjhiblot@ti.com>
2017-12-14 21:55:23 +0000

    netboot-initramfs: Make it possible to use NFS as the rootfs with non-builtin drivers
    USB drivers are usually built as modules. This prevents using NFS as the
    rootfs over a USB because the in-kernel support is not yet available at
    the end of the kernel init.
    
    To circumvent the problem, a minimal initramfs can be used to probe the
    missing modules and finally setup the NFS mount.
    
    The initramfs contains the required modules, udev and a
    init script.
    The script's actions are (in execution order):
    * starts udev and trigger udev's enumeration
    * parses the kernel command line to extract the NFS kernel options
    * configure the ethernet device
    * mount the NFS
    * execute switch_root
    
    The list of kernel module is defined by the variable NETBOOT_KERNEL_MODULES.
    It contains the list of modules separated by space without the ".ko"
    extension.
    
    Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>
    Signed-off-by: Denys Dmytriyenko <denys@ti.com>

---
+++ meta-arago-distro/recipes-bsp/netboot-initrd/files/init-netboot.sh
@@ -0,0 +1,132 @@
+#!/bin/sh
+
+export PATH=$PATH:/sbin:/usr/sbin
+
+early_setup() {
+    mkdir -p /proc /sys /tmp /run
+    mount -t proc proc /proc
+    mount -t sysfs sysfs /sys
+    mount -t devtmpfs none /dev
+}
+
+beginswith() { case $2 in "$1"*) true;; *) false;; esac; }
+
+read_args() {
+    for arg in $2; do
+        optarg=`expr "x$arg" : 'x[^=]*=\(.*\)'`
+        if beginswith $1 $arg ; then
+		echo  $optarg
+		break
+	fi
+    done
+}
+
+parse_server_info() {
+    info=$1
+    ip=$(echo $info | cut -s -d':' -f1 )
+    info=${info#$ip:}
+    path=$(echo $info | cut -d',' -f1 )
+    info=${info#$path}
+    options=${info#,}
+
+    eval "$2=$ip"
+    eval "$3=$path"
+    eval "$4=$options"
+}
+
+do_dhcp() {
+	if [ "$1" != "" ] ; then
+		udhcpc -i $1
+	else
+		udhcpc
+	fi
+}
+
+configure_eth_dev() {
+	conf=$1
+
+	client_ip=$(echo $conf | cut -s -d':' -f 1)
+	server_ip=$(echo $conf | cut -s -d':' -f 2)
+	gw_ip=$(echo $conf | cut -s -d':' -f 3)
+	netmask=$(echo $conf | cut -s -d':' -f 4)
+	hostname=$(echo $conf | cut -s -d':' -f 5)
+	device=$(echo $conf | cut -s -d':' -f 6)
+	autoconf=$(echo $conf | cut -s -d':' -f 7)
+	dns0_ip=$(echo $conf | cut -s -d':' -f 8)
+	dns1_ip=$(echo $conf | cut -s -d':' -f 9)
+
+	if [ "$device" == "" ]; then
+		device="eth0";
+	fi
+
+	echo waiting for device $device to come up
+	while [ -z "$(ip link show $device 2> /dev/null)" ] ; do
+		echo .;
+		sleep 1
+	done
+	echo done
+
+
+	ip link set $device up
+	echo waiting establishment of link on $device
+	while [ -z "$(ip link show $device | grep '<*LOWER_UP*>')" ] ; do
+		echo .;
+		sleep 1
+	done
+	echo done
+
+	#handle the case 'ip=dhcp'
+	if [ "$conf" == "dhcp" ] ; then
+		do_dhcp
+		return $?
+	fi
+
+	#handle the 'autoconf' case
+	if [ "$autoconf" == "dhcp" ] ; then
+		do_dhcp $device
+		return $?
+	fi
+
+	if [ "$netmask" != "" ]; then
+		ip addr add $client_ip/$netmask dev $device
+	else
+		ip addr add $client_ip dev $device
+	fi
+
+	if [ "$gw_ip" != "" ] ; then
+		ip route add default via $gw_ip
+	fi
+	if [ "$dns0_ip" != "" ] ; then
+		echo $dns0_ip >> /etc/resolv.conf
+	fi
+	if [ "$dns1_ip" != "" ] ; then
+		echo $dns1_ip >> /etc/resolv.conf
+	fi
+}
+
+early_setup
+udevd --daemon
+udevadm trigger
+udevadm settle
+
+cmdline=$(cat /proc/cmdline)
+
+root_dev=$(read_args  root "$cmdline")
+server_info=$(read_args nfsroot "$cmdline")
+ip_info=$(read_args ip "$cmdline")
+
+if [ "$root_dev" != "/dev/nfs" ]; then
+	echo "invalid root device. It must be /dev/nfs"
+	exit 1
+fi
+
+#
+configure_eth_dev $ip_info
+parse_server_info $server_info ip path options
+
+# mount NFS drive
+mkdir /newroot
+mount -t nfs -o nolock,rw,$options $ip:$path /newroot
+
+# switch root. new root is the NFS mount
+exec switch_root /newroot /sbin/init
---
+++ meta-arago-distro/recipes-bsp/netboot-initrd/netboot-initrd_0.0.1.bb
@@ -0,0 +1,46 @@
+SUMMARY = "Initramfs image for NFS boot"
+DESCRIPTION = "Small image capable of mounting a newtwork root filesystem (NFS)"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+inherit core-image image_types
+
+#by default include the kernel modules necessary to use a ASIX USB/Ethernet adapter
+NETBOOT_KERNEL_MODULES ?= "dwc3 dwc3-omap asix xhci-plat-hcd extcon-usb-gpio"
+
+export IMAGE_BASENAME = "netboot-initramfs"
+
+#generate an image that can be loaded by u-boot
+IMAGE_FSTYPES = "${INITRAMFS_FSTYPES} ${INITRAMFS_FSTYPES}.u-boot"
+
+# include udev (used to load the kernel modules)
+VIRTUAL-RUNTIME_dev_manager = "udev"
+
+# List of packages to install
+IMAGE_INSTALL = "\
+  bash \
+  busybox \
+  udev \
+  netboot-initrd-scripts\
+  "
+
+# add the required kernel modules
+IMAGE_INSTALL += "${@' '.join(['kernel-module-'+x for x in d.getVar('NETBOOT_KERNEL_MODULES',d,1).split()])}"
+
+
+# Reduce the size of the rootfs by removing features
+
+# Do not include buysbox's syslog
+BAD_RECOMMENDATIONS += "busybox-syslog"
+# Do not pollute the initrd image with rootfs features
+IMAGE_FEATURES = ""
+# Keep extra language files from being installed
+IMAGE_LINGUAS = ""
+
+# To further reduce the size of the rootfs, remove the /boot directory from the final image
+# this is usually done by adding RDEPENDS_kernel-base = "" in the configuration file. In our case we can't use
+# this method. Instead we just wipe out the content of "/boot" before creating the image.
+ROOTFS_POSTPROCESS_COMMAND += 'empty_boot_dir'
+empty_boot_dir () {
+	rm -rf ${IMAGE_ROOTFS}/boot/*
+}
