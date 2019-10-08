2fea8e5df4a58bfc4c6e4fe91e2a93ff3703c9c1
Hongmei Gou <a0271529@ti.com>
2019-10-08 04:29:20 +0000

    tiovx-app-host: TI OpenVX IPC HOST implementation and sample applications
    * The sample applications include running OpenVX conformance tests
      as well as a Khronos tutorial example
    
    Signed-off-by: Djordje Senicic <x0157990@ti.com>
    Signed-off-by: Jacob Stiffler <j-stiffler@ti.com>
    Signed-off-by: Hongmei Gou <a0271529@ti.com>
    Signed-off-by: Denys Dmytriyenko <denys@ti.com>

---
+++ meta-arago-extras/recipes-ti/tiovx/files/setenv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PROC_LIST='host khronos_example'
+
+export LINUX_SYSROOT_DIR=/
+export TIOVXPATH=/usr/include/VX
+export IPCPATH=/usr/share/ti/ti-ipc-tree
+export SDKPLATFORMIFPATH=/usr/share/ti/ti-tiovx-lib-tree/sys-iface
