b4e71a99339209f559f42f1f7775abea3ed0865a
e-ruei1@ti.com <e-ruei1@ti.com>
2019-10-31 01:55:45 +0000

    qtbase-conf: set the default EGLFS mode to kms
    We start to use the Mesa-based EGL 1.5-enabled GLES/EGL libraries where
    NullDRM window system is no longer supported and GBM(DRM) window system
    should be used instead for full-screen operation.
    
    Signed-off-by: Eric Ruei <e-ruei1@ti.com>
    Signed-off-by: Denys Dmytriyenko <denys@ti.com>

--- meta-arago-distro/recipes-qt/qt5/qtbase-conf/qt_env.sh
+++ meta-arago-distro/recipes-qt/qt5/qtbase-conf/qt_env.sh
@@ -3,5 +3,5 @@
 ### QT Environment Variables ###
 export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
 export QT_QPA_EGLFS_KMS_CONFIG=/etc/qt5/eglfs_kms_cfg.json
-export QT_QPA_EGLFS_INTEGRATION=none
+export QT_QPA_EGLFS_INTEGRATION=eglfs_kms
 export QT_WAYLAND_SHELL_INTEGRATION=wl-shell
--- meta-arago-distro/recipes-qt/qt5/qtbase-conf/ti33x/qt_env.sh
+++ meta-arago-distro/recipes-qt/qt5/qtbase-conf/ti33x/qt_env.sh
@@ -4,5 +4,5 @@
 export QT_QPA_EVDEV_TOUCHSCREEN_PARAMETERS="rotate=180"
 export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
 export QT_QPA_EGLFS_KMS_CONFIG=/etc/qt5/eglfs_kms_cfg.json
-export QT_QPA_EGLFS_INTEGRATION=none
+export QT_QPA_EGLFS_INTEGRATION=eglfs_kms
 export QT_WAYLAND_SHELL_INTEGRATION=wl-shell
