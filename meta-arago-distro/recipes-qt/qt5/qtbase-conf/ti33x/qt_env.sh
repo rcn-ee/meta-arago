068585973f5403d67ae7c9414efcab64583fc72d
Vivien Didelot <vivien.didelot@gmail.com>
2021-10-07 13:57:29 -0400

    qtbase-conf: disable the seccomp filter sandbox
    SECCOMP-BPF Sandbox is known to cause runtime issues so set the
    QTWEBENGINE_CHROMIUM_FLAGS environment variable to disable this
    feature temporarily until those issues are resolved.
    
    Signed-off-by: Vivien Didelot <vdidelot@pbsc.com>
    Acked-by: Denys Dmytriyenko <denys@konsulko.com>
    Signed-off-by: Yogesh Siraswar <yogeshs@ti.com>

--- meta-arago-distro/recipes-qt/qt5/qtbase-conf/ti33x/qt_env.sh
+++ meta-arago-distro/recipes-qt/qt5/qtbase-conf/ti33x/qt_env.sh
@@ -7,3 +7,8 @@ export QT_QPA_EGLFS_KMS_CONFIG=/etc/qt5/eglfs_kms_cfg.json
 export QT_QPA_EGLFS_INTEGRATION=eglfs_kms
 export QT_QPA_EGLFS_ALWAYS_SET_MODE=1
 export QT_WAYLAND_SHELL_INTEGRATION=wl-shell
+
+# SECCOMP-BPF Sandbox does not work due to unexpected FUTEX_UNLOCK_PI call
+# from the pthread implementation. Disable this feature temporarily until
+# those issues are resolved.
+export QTWEBENGINE_CHROMIUM_FLAGS="--disable-seccomp-filter-sandbox"
